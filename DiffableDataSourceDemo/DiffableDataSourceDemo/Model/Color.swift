039aef1b3c0acf830b413ca0418c7d9062520ff5
RyanGreenburg <>
2020-09-30 23:51:18 -0600

    Constructs model

--- DiffableDataSourceDemo/DiffableDataSourceDemo/Model/Color.swift
+++ DiffableDataSourceDemo/DiffableDataSourceDemo/Model/Color.swift
@@ -5,4 +5,16 @@
 //  Created by RYAN GREENBURG on 9/30/20.
 //
 
-import Foundation
+import UIKit
+
+struct Color: Hashable {
+    let name: String
+    let color: UIColor
+    let id: String
+    
+    init(color: UIColor) {
+        name = String(color.description)
+        self.color = color
+        id = UUID().uuidString
+    }
+}
